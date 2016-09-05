3a9f2f77cc151ed0f4f5d72c4f375e7cc294fbff
hugosp <stefan.sundell@gmail.com>
2016-09-05 03:22:44 +0000

    trött

--- public/script.js
+++ public/script.js
@@ -1,12 +1,22 @@
+/* global $ io */
+
 $(function() {
     var socket = io();
-   
+    
+    var $stock = $('#addStock');
     
     socket.on('connect',function(data){
         console.log('connected to server');
         
         socket.on('message',function(msg){
             console.log(msg);
+            if(msg.action == 'add') {
+                $('#stockCards').append('<li id="'+msg.stock+'">'+msg.stock+'</li>');
+                $('#cardHolder').appendTemplate("templates/card.html", {name: "GOOG", info: "Google blah blah"});
+
+            } else if(msg.action == 'remove') {
+                $('#'+msg.stock).remove();
+            }
         });
 
     });
@@ -14,8 +24,8 @@ $(function() {
     
     $('#test').on('click',function(){ getData('S','2001-01-01','2002-01-01') });
     
-    $('#add').on('click',function(){ socket.emit('message', 'adding'); });
-    $('#remove').on('click',function(){ socket.emit('message', 'removing'); });         
+    $('#add').on('click',function(){ socket.emit('message', {action:'add',stock: $stock.val()}); });
+    $('#remove').on('click',function(){ socket.emit('message', {action:'remove',stock: $stock.val()}); });         
 
 });
 
